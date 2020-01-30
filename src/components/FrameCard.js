0a14908fc9b24afc25dcd2f700725531b318c174
Snickerson12 <laura.nickerson@gmail.com>
2020-01-30 16:28:33 -0600

    added custom card formatting, checkbox, and color change on click to frames. still need implementation of specifying which card is clicked

--- src/components/FrameCard.js
+++ src/components/FrameCard.js
@@ -2,21 +2,54 @@ import React, {Component} from 'react';
 import { Card, Checkbox } from 'semantic-ui-react';
 
 export default class FrameCard extends Component{
+    constructor() {
+        super()
+        this.state = {
+            owned: false,
+            bgColor: 'white',
+        }
+    }
+
+    handleClick = (f) => {
+        if(this.state.owned === false) {
+            this.setState({
+                owned: true,
+                bgColor: 'teal'
+            })
+        }else{
+            this.setState({
+                owned: false,
+                bgColor: 'white'
+            })
+        }
+    }
+
+    renderCard = (f, index) => {
+        return(
+            <div className='frame-card-container' onClick={() => this.handleClick(f)} style = {{backgroundColor: this.state.bgColor}}>
+                <div>
+                    <div><img src={f.wikiaThumbnail} className='frame-image'/></div>
+                    <div className="header">{f.name}</div>
+                    <div className="description"> {f.description} </div>
+                    <form className='checkbox'>
+                        <label> Mastered? </label>
+                        <input type='checkbox'/>
+                    </form>
+                </div>
+            </div>
+        )
+    }
    
     render() {
-        console.log(this.props.frames)
-        if(this.props.frames) {
-            return(
-                this.props.frames.map(f => 
-                    <div className='card-container'>
-                        <div>
-                            <div><img src={f.wikiaThumbnail} className='frame-image'/></div>
-                            <div className="header">{f.name}</div>
-                            <div className="description"> {f.description} </div>
-                        </div>
-                    </div> 
-                )
-            )       
-        } else {return <div>Loading...</div>}
+        return(
+            <div className='frame-card'>
+                {this.props.frames
+                ?
+                    this.props.frames.map((f, index) => this.renderCard(f, index))
+                :
+                    <div>Loading</div>
+                }
+            </div>  
+        )       
     }
 }

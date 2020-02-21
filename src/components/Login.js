01281bf6a58e2cc07fa340dbd785b32564391a7e
Snickerson12 <laura.nickerson@gmail.com>
2020-02-20 19:29:57 -0600

    basic formatting for login screen

--- src/components/Login.js
+++ src/components/Login.js
@@ -18,13 +18,13 @@ export default class Login extends Component {
     render() {
         return(
             <div className='login-page'>
-                <div>User Login</div>
-                <form>
-                    <label>Username </label>
-                    <input type="text" name="username" onChange={this.handleChange}/>
-                    <label> Password </label>
-                    <input type="email" name="email" onChange={this.handleChange}/>
-                    <input type="submit" value="submit" />
+                <div id='login-text'>User Login</div>
+                <form id='login-form'>
+                    <label id='form-label'>Username </label>
+                    <input type="text" name="username" onChange={this.handleChange} id='form-input'/>
+                    <label id='form-label'> Password </label>
+                    <input type="email" name="email" onChange={this.handleChange} id='form-input'/>
+                    <input type="submit" value="submit" className='login-submit' />
                 </form>
             </div>
         )

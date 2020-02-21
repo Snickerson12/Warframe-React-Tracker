80e785a30cba7a1e4d16a515b3920130d1a1073a
Snickerson12 <laura.nickerson@gmail.com>
2020-02-20 19:16:45 -0600

    login inputs added. unformatted but with onchange event listeners

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,31 @@
 import React, {Component} from 'react';
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            input: ''
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+        console.log(this.state.input)
+    }
+
     render() {
         return(
             <div className='login-page'>
-                Login
+                <div>User Login</div>
+                <form>
+                    <label>Username </label>
+                    <input type="text" name="username" onChange={this.handleChange}/>
+                    <label> Password </label>
+                    <input type="email" name="email" onChange={this.handleChange}/>
+                    <input type="submit" value="submit" />
+                </form>
             </div>
         )
     }

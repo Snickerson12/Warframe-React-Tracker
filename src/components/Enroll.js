c65d2dc5de97308ac2d7b4eeb27f829f5f33897c
Snickerson12 <laura.nickerson@gmail.com>
2020-02-21 20:18:27 -0600

    sign up page form and styling

--- src/components/Enroll.js
+++ src/components/Enroll.js
@@ -3,8 +3,16 @@ import React, {Component} from 'react';
 export default class Enroll extends Component {
     render() {
         return(
-            <div>
-                enroll
+            <div className='login-page'>
+                <div id='login-text'>Create an Account</div>
+                <p> We recommend using your in-game name as your username</p>
+                <form id='login-form'>
+                    <label id='form-label'>Username </label>
+                    <input type="text" name="username" onChange={this.handleChange} id='form-input'/>
+                    <label id='form-label'> Password </label>
+                    <input type="email" name="email" onChange={this.handleChange} id='form-input'/>
+                    <input type="submit" value="submit" className='login-submit' />
+                </form>
             </div>
         )
     }
